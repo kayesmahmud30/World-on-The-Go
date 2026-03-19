90d3a0f6614d476ce9e1759a720ceff50594f5dd
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-03-20 03:44:38 +0600

    Added some new file and sections with some folders & files

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./Country.css";
+
+const Country = ({ country }) => {
+  //   console.log(country.area.area);
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    //------1st way.....
+    // if(visited){
+    //     setVisited(false);
+    // } else{
+    //     setVisited(true);
+    // }
+
+    //------2st way.....
+    // setVisited(visited ? false : true);
+
+    //------3st way.....
+    setVisited(!visited);
+  };
+
+  return (
+    // <div
+    //   className={`country ${visited ? "country-visited" : "country-not-visited"}`}
+    // >
+
+    <div className={`country ${visited && "country-visited"}`}>
+      <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+      <h3>Name: {country.name.common}</h3>
+      <p>Population: {country.population.population} </p>
+      <p>
+        Area: {country.area.area} |{" "}
+        {country.area.area > 300000 ? "Big country" : "Small Country"}
+      </p>
+
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
+    </div>
+  );
+};
+
+export default Country;
+
+/*
+-----> 4 Ways to add css in jsx file react....
+
+1. Inline CSS (Style Object)
+2. 
+
+*/
